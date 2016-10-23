42f4f73db835794118fd859d1fdff00caf723993
Erick <sr.tama@outlook.com>
2016-10-22 23:28:24 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name = "mcpi",
     version = "1.0.1",
     author = "Martin O'Hanlon",
+    author_email="sr.tama@outlook.com",
     description = ("Refer to http://www.stuffaboutcode.com"),
     keywords = "raspberry pi minecraft python tutorial",
     url = "https://github.com/MrMenezes/mcpi",
