c24696e060287b7fdccf9a098b467a502bfeba27
BuildTools <unconfigured@null.spigotmc.org>
2016-10-23 00:11:36 -0300

    Remove setup.cfg

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
+import os
 from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "mcpi",
-    version = "1.0.1.1",
+    version = "1.0.1.2",
     author = "Martin O'Hanlon",
     author_email="sr.tama@outlook.com",
     description = ("Refer to http://www.stuffaboutcode.com"),
     keywords = "raspberry pi minecraft python tutorial",
     url = "https://github.com/MrMenezes/mcpi",
     packages=['mcpi'],
+    long_description=read('README.md'),
 )
