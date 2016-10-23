d2e8f5110eafc6be54f95906c07f28961a6a8ced
Erick <sr.tama@outlook.com>
2016-10-22 23:23:26 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name = "mcpi",
     version = "1.0.1",
-    author = "brooksc",
-    description = ("Refer to mcpipy.com"),
+    author = "Martin O'Hanlon",
+    description = ("Refer to http://www.stuffaboutcode.com"),
     keywords = "raspberry pi minecraft python tutorial",
     url = "https://github.com/MrMenezes/mcpi",
     packages=['mcpi'],
