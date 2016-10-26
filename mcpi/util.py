617fdc212388a305d206e97bdc7ee508e0daa6ff
BuildTools <unconfigured@null.spigotmc.org>
2016-10-26 14:30:06 -0300

    Change basestring to bytes and encode utf-8

--- mcpi/util.py
+++ mcpi/util.py
@@ -2,7 +2,7 @@ import collections
 
 def flatten(l):
     for e in l:
-        if isinstance(e, collections.Iterable) and not isinstance(e, basestring):
+        if isinstance(e, collections.Iterable) and not isinstance(e, bytes):
             for ee in flatten(e): yield ee
         else: yield e
 
